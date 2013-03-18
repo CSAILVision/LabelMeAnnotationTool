7aa3c70fb173dbc119a4591e3731a7715a63ef78
michael <insominx@yahoo.com>
2013-03-18 11:18:46 -0700

    added new imageAttribute object type

---
+++ annotationTools/js/image_attribute.js
@@ -0,0 +1,69 @@
+
+
+// Created: 03//2013
+
+// image attribute class
+// Keeps track of all information related to an individual image attribute
+function imageAttribute(attribute_id) {
+
+  // *******************************************
+  // Private variables:
+  // *******************************************
+
+  this.id;
+  this.attribute_name = '';
+  this.username = 'anonymous';
+  this.deleted = 0;
+  this.verified = 0;
+  this.attribute_id = attribute_id;
+  this.div_attach = 'myCanvas_bg';
+  this.CloseErrorFlag = 0;
+
+  // *******************************************
+  // Public methods:
+  // *******************************************
+
+  this.SetID = function(id) {
+    this.id = id;
+  };
+
+  this.GetAttributeName = function () {
+    return this.obj_name;
+  };
+
+  this.SetAttributeName = function(name) {
+    this.obj_name = name;
+  };
+
+  this.GetUsername = function () {
+    return this.username;
+  };
+
+  this.SetUsername = function(u) {
+    this.username = u;
+  };
+
+  this.GetDeleted = function () {
+    return this.deleted;
+  };
+
+  this.SetDeleted = function(d) {
+    this.deleted = d;
+  };
+
+  this.GetVerified = function () {
+    return this.verified;
+  };
+
+  this.SetVerified = function (v) {
+    this.verified = v;
+  };
+
+  this.SetDivAttach = function(da) {
+    this.div_attach = da;
+  };
+
+  this.GetAttributeID = function () {
+    return this.attribute_id;
+  };
+}
