9846b7272f743de3dfdc6839ac933087d5682e8f
brussell123 <bcr@cs.washington.edu>
2013-06-18 11:25:41 -0700

    added input/output functions

---
+++ annotationTools/js/io.js
@@ -0,0 +1,9 @@
+function ReadXML(xml_file,SuccessFunction,ErrorFunction) {
+  $.ajax({
+    type: "GET",
+    url: xml_file,
+    dataType: "xml",
+    success: SuccessFunction,
+    error: ErrorFunction
+  });
+}
