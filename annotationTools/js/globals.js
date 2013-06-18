a5733f9c3868ec5988a7dd5187cefb63c9e42c85
brussell123 <bcr@cs.washington.edu>
2013-06-18 08:31:43 -0700

    added jquery, global variables, and sign in form modules

---
+++ annotationTools/js/globals.js
@@ -0,0 +1,10 @@
+// This file contains global variables used in LabelMe.
+
+// Parsed LabelMe XML file. Manipulate this variable with jquery.
+var LM_xml;
+
+// LabelMe username:
+var username = 'anonymous';
+
+// Boolean indicating whether user is currently signing in (this should be abstracted into class):
+var username_flag = 0;
