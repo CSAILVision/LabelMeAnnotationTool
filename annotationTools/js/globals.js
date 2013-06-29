91fba4d8c5f03e3b6b923313a704c1ed8d330022
Antonio Torralba <torralba@labelme2.csail.mit.edu>
2013-06-29 13:31:09 -0400

    Antonio changes

--- annotationTools/js/globals.js
+++ annotationTools/js/globals.js
@@ -8,3 +8,6 @@ var username = 'anonymous';
 
 // Boolean indicating whether user is currently signing in (this should be abstracted into class):
 var username_flag = 0;
+
+// Boolean indicating if we will use attributes. This should be read from the URL and set to 0 by default.
+var use_attributes = 1; // if this is 0, then it will remove all the attributes from the bubble.
