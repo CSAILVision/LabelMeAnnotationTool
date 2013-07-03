ac6a28e2828bad3a8bce0f7951377e84a5273bcd
Antonio Torralba <torralba@labelme2.csail.mit.edu>
2013-07-03 19:26:26 -0400

    zoom fixed

--- annotationTools/js/globals.js
+++ annotationTools/js/globals.js
@@ -14,3 +14,6 @@ var username_flag = 0;
 
 // Boolean indicating if we will use attributes. This should be read from the URL and set to 0 by default.
 var use_attributes = 1; // if this is 0, then it will remove all the attributes from the bubble.
+
+// for now, let's remove the attributes in MT mode. Just in case anybody is trying this.
+if (getQueryVariable('mode')=='mt'){use_attributes=0;}
