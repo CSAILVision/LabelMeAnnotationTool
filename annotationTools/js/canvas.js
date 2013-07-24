01914eb3e455c34a0b1083027854c4e6c040bf83
brussell123 <bcr@cs.washington.edu>
2013-07-24 07:54:26 -0700

    changed indentation

--- annotationTools/js/canvas.js
+++ annotationTools/js/canvas.js
@@ -9,64 +9,64 @@ function canvas(div_attach) {
 //   this.annotations; // includes name, deleted, verified info
   this.div_attach = div_attach; // name of DIV element to attach to
     
-    // *******************************************
-    // Public methods:
-    // *******************************************
-    
-    // Returns all of the annotations as an array.
-    this.GetAnnotations = function () {
-      return AllAnnotations;
-    };
-    
-    // Loop through all of the annotations and draw the polygons.
-    this.DrawAllPolygons = function () {
-      for(var pp=0; pp < this.GetAnnotations().length; pp++) {
-	var isDeleted = this.GetAnnotations()[pp].GetDeleted();
-	if(((pp<num_orig_anno)&&((view_Existing&&!isDeleted)||(isDeleted&&view_Deleted))) || (pp>=num_orig_anno)) {
-	  this.GetAnnotations()[pp].DrawPolygon(main_image.GetImRatio());
-          
-	  // *****************************************
-	  this.GetAnnotations()[pp].SetAttribute('onmousedown','main_handler.RestToSelected(' + pp + ',evt); return false;');
-	  this.GetAnnotations()[pp].SetAttribute('onmousemove','main_handler.CanvasMouseMove(evt,'+ pp +'); return false;');
-	  this.GetAnnotations()[pp].SetAttribute('oncontextmenu','return false');
-	  this.GetAnnotations()[pp].SetAttribute('style','cursor:pointer;');
-	  // *****************************************
-	}
-      }
-    };
-    
-    // Loop through all of the annotations and clear them from the canvas.
-    this.ClearAllAnnotations = function () {
-        for(var i=0;i<this.GetAnnotations().length;i++) {
-            this.GetAnnotations()[i].DeletePolygon();
-        }
-    };
-    
-    // Add a new annotation to the canvas.
-    this.AddAnnotation = function (anno) {
-        this.GetAnnotations().push(anno);
-        this.AttachAnnotation(anno);
-    };
-    
-    // Attach the annotation to the canvas.
-    this.AttachAnnotation = function (anno) {
-        if(anno.GetDeleted()&&(!view_Deleted)) return;
-        anno.SetDivAttach(this.div_attach);
-        anno.DrawPolygon(main_image.GetImRatio());
+  // *******************************************
+  // Public methods:
+  // *******************************************
+  
+  // Returns all of the annotations as an array.
+  this.GetAnnotations = function () {
+    return AllAnnotations;
+  };
+  
+  // Loop through all of the annotations and draw the polygons.
+  this.DrawAllPolygons = function () {
+    for(var pp=0; pp < this.GetAnnotations().length; pp++) {
+      var isDeleted = this.GetAnnotations()[pp].GetDeleted();
+      if(((pp<num_orig_anno)&&((view_Existing&&!isDeleted)||(isDeleted&&view_Deleted))) || (pp>=num_orig_anno)) {
+	this.GetAnnotations()[pp].DrawPolygon(main_image.GetImRatio());
         
-        // *****************************************
-        var anno_id = anno.GetAnnoID();
-        anno.SetAttribute('onmousedown','main_handler.RestToSelected(' + anno_id + ',evt); return false;');
-        anno.SetAttribute('onmousemove','main_handler.CanvasMouseMove(evt,' + anno_id + ');');
-        anno.SetAttribute('oncontextmenu','return false');
-        anno.SetAttribute('style','cursor:pointer;');
-        // *****************************************
-    };
+	// *****************************************
+	this.GetAnnotations()[pp].SetAttribute('onmousedown','main_handler.RestToSelected(' + pp + ',evt); return false;');
+	this.GetAnnotations()[pp].SetAttribute('onmousemove','main_handler.CanvasMouseMove(evt,'+ pp +'); return false;');
+	this.GetAnnotations()[pp].SetAttribute('oncontextmenu','return false');
+	this.GetAnnotations()[pp].SetAttribute('style','cursor:pointer;');
+	// *****************************************
+      }
+    }
+  };
+  
+  // Loop through all of the annotations and clear them from the canvas.
+  this.ClearAllAnnotations = function () {
+    for(var i=0;i<this.GetAnnotations().length;i++) {
+      this.GetAnnotations()[i].DeletePolygon();
+    }
+  };
+  
+  // Add a new annotation to the canvas.
+  this.AddAnnotation = function (anno) {
+    this.GetAnnotations().push(anno);
+    this.AttachAnnotation(anno);
+  };
+  
+  // Attach the annotation to the canvas.
+  this.AttachAnnotation = function (anno) {
+    if(anno.GetDeleted()&&(!view_Deleted)) return;
+    anno.SetDivAttach(this.div_attach);
+    anno.DrawPolygon(main_image.GetImRatio());
     
-    // Detach annotation from the canvas.
-    this.DetachAnnotation = function(anno_id) {
-        var anno = this.GetAnnotations()[anno_id];
-        anno.DeletePolygon();
-        return anno;
-    };
+    // *****************************************
+    var anno_id = anno.GetAnnoID();
+    anno.SetAttribute('onmousedown','main_handler.RestToSelected(' + anno_id + ',evt); return false;');
+    anno.SetAttribute('onmousemove','main_handler.CanvasMouseMove(evt,' + anno_id + ');');
+    anno.SetAttribute('oncontextmenu','return false');
+    anno.SetAttribute('style','cursor:pointer;');
+    // *****************************************
+  };
+  
+  // Detach annotation from the canvas.
+  this.DetachAnnotation = function(anno_id) {
+    var anno = this.GetAnnotations()[anno_id];
+    anno.DeletePolygon();
+    return anno;
+  };
 }
