57289bb8077112f7db15d966b724a1b8900efeab
Bryan Russell <bcr@cs.washington.edu>
2015-03-11 15:53:08 -0700

    set up first LM unit test loading XML file data

--- annotationTools/js/tests.js
+++ annotationTools/js/tests.js
@@ -1,3 +1,16 @@
 QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+    assert.ok( 1 == "1", "Passed!" );
+});
+
+QUnit.test( "ReadXML test", function( assert ) {
+    var anno_file = "Annotations/example_folder/img1.xml"
+    var SuccessFunction = function(xml) {
+      console.log("Successfully loaded XML file");
+      console.log(xml);
+    };
+    var FailureFunction = function() {
+      console.log("Failed to load XML file");
+    };
+    ReadXML(anno_file,SuccessFunction,FailureFunction);
+    assert.ok( 1 == "1", "Passed!" );
 });
